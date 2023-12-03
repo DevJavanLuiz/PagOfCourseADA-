824b46d32d9f5d810bc0538e8a00bc1870d4c932
Luizinho <javanlsj@gmail.com>
2023-12-03 16:32:52 -0300

    tentando utilizar o js na troca de cores dos btn's

--- siteADA/assets/js/scrip.js
+++ siteADA/assets/js/scrip.js
@@ -1,4 +1,25 @@
-document.querySelector('.mudar-cor').addEventListener('click', (e) =>{
-    e.target.classList.toggle('.mudar-cor')
-    document.body.classList.toggle('dark');
-})
+
+
+
+function inverterCor() {
+    let span = document.querySelectorAll('span');
+    for (let i = 0; i < span.length; i++) {
+        span[i].classList.toggle('mudar-cor-click');
+    }
+
+}
+
+const btnlogin = document.querySelector('.btn-login');
+const btnMudarCor = document.querySelector('.mudar-cor');
+
+let corAtualRosa = '#FF5678';
+
+btnMudarCor.addEventListener('click'), function () {
+    if (corAtualRosa === '#FF5678') {
+        corAtualRosa = '#35E6E9';
+    } else {
+        corAtualRosa = '#FF5678';
+    }
+
+    btnlogin.style.backgroundColor = corAtualRosa;
+};
